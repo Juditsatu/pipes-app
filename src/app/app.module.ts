72d366777550f9827fb2c2c4fe5ea06d8b3eb3e5
Judit Santos <judit.sant@gmail.com>
2022-08-01 22:36:41 +0200

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRouterModule } from './app-router.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+//Cambia el locale de la app
+import localeCa from '@angular/common/locales/ca-AD';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData( localeCa );
+
 
 @NgModule({
   declarations: [
@@ -12,10 +20,14 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRouterModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'ca-AD' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  // i18nSelect
+  nombre: string = 'Judit';
+  genero: string = 'femenino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+  
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Clara'];
+  clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+
+  }
+
+  cambiarCliente() {
+    this.nombre = 'Fernando';
+    this.genero = 'masculino';
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
   }
 
 }
